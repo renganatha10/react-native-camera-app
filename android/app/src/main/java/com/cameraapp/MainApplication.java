4b3afedff6a987ab92e527ee02d45f3e4d2e92dc
Renganatha.A <renganatha@geekyants.com>
2019-02-14 15:25:15 +0530

    Add Assets, FFMPEG Library RNFS Library

--- android/app/src/main/java/com/cameraapp/MainApplication.java
+++ android/app/src/main/java/com/cameraapp/MainApplication.java
@@ -3,6 +3,8 @@ package com.cameraapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
+import com.arthenica.reactnative.RNFFmpegPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFSPackage(),
+            new RNFFmpegPackage(),
             new ReactVideoPackage(),
             new RNCameraPackage()
       );
