d1643c3e31701d6aa8b4d400eabc13c4a10aa8b4
Renganatha.A <renganatha@geekyants.com>
2019-02-14 22:37:11 +0530

    Complete the App Flow, Save the Edited Video

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-/**
- * @format
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
-
 import { AppRegistry } from 'react-native';
-import App from './Ffmpeg';
+import App from './src';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
